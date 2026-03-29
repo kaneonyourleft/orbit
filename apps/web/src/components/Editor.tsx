b34e4828deffd02811ba7bd0a089ef7263ab4113
KaneGong <kane.onyourleft@gmail.com>
2026-03-29 18:00:26 +0900

    auto: deploy from file watcher

--- apps/web/src/components/Editor.tsx
+++ apps/web/src/components/Editor.tsx
@@ -4,27 +4,23 @@ import "@blocknote/mantine/style.css";
 import { BlockNoteSchema, defaultBlockSpecs } from "@blocknote/core";
 import { useCreateBlockNote } from "@blocknote/react";
 import { BlockNoteView } from "@blocknote/mantine";
-import {
-  SuggestionMenuController,
-  getDefaultReactSlashMenuItems,
-} from "@blocknote/react";
+import { SuggestionMenuController, getDefaultReactSlashMenuItems } from "@blocknote/react";
 import { TwoColumns, ThreeColumns, FourColumns, Divider, Spacer } from "./blocks/ColumnsBlock";
 import { layoutSlashMenuItems } from "./blocks/slashMenuItems";
 
 const schema = BlockNoteSchema.create({
   blockSpecs: {
     ...defaultBlockSpecs,
-    twoColumns: TwoColumns,
-    threeColumns: ThreeColumns,
-    fourColumns: FourColumns,
-    divider: Divider,
-    spacer: Spacer,
+    twoColumns: TwoColumns(),
+    threeColumns: ThreeColumns(),
+    fourColumns: FourColumns(),
+    divider: Divider(),
+    spacer: Spacer(),
   },
 });
 
 export default function Editor() {
   const editor = useCreateBlockNote({ schema });
-
   return (
     <BlockNoteView editor={editor} slashMenu={false} theme="light">
       <SuggestionMenuController
